10d9869e449bbf54ab486c20985f645b09eb29ab
Ayush Singh <148626222+ayushsingh-01@users.noreply.github.com>
2026-02-27 15:29:46 +0530

    model for course and student

---
+++ backend/src/models/Course.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  courseName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  courseDescription: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  instructor: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Course", courseSchema);
